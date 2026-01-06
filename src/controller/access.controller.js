71cf0c3c49d9389c0aaaf9a596c9f7abe7eb96a0
son <sont9779@gmail.com>
2026-01-06 23:55:28 +0700

    add controller access

--- src/controller/access.controller.js
+++ src/controller/access.controller.js
@@ -1,5 +1,6 @@
 const accessService = require("../services/access.service");
 
+// Controller for handling access-related requests
 class AccessController {
   signUp = async (req, res, next) => {
     return res.status(201).json(await accessService.signUp(req.body));
